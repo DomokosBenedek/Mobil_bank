d5c94bf7e74635c93e06e4713883f7824bf2fa5a
DomokosBenedek <75673497+DomokosBenedek@users.noreply.github.com>
2025-01-19 14:50:04 +0100

    base_design

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './footer.css'
+import './navbar.css'
 import HomePage from './components/home.tsx'
 
 createRoot(document.getElementById('root')!).render(
