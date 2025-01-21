c90d65eb54fddfe933e8a40558529f550af8787f
Domokos Benedek <benedekdomokos50@gmail.com>
2025-01-21 09:57:52 +0100

    advantages_texts

--- frontend/src/components/Home_main.tsx
+++ frontend/src/components/Home_main.tsx
@@ -29,9 +29,59 @@ export function Function() {
 }
 export function Advantages() {  
     return (
-      <>
+      <section className="advantages">
+
+        <div>
+          {/* Advantages text */}
+          <div className="advantages-text">
+            <h2>Kövesd nyomon pénzügyeidet egyszerűen és gyorsan</h2>
+            <p>
+              Ez a mobilbanki alkalmazás lehetővé teszi, hogy könnyedén nyomon
+              kövesd bevételeidet és kiadásaidat. Használatával egyszerűsítheted a
+              pénzügyeid kezelését, és mindig tudni fogod, hol állsz anyagilag.
+            </p>
+          </div>
+
+          {/* Advantages cards */}
+          <div className="advantages-cards">
+            <div className="advantage-card">
+              <img src="/path/to/advantage-icon-1.png" alt="Advantage Icon" />
+              <h6>Egyszerű kezelés</h6>
+              <p>
+                A mobilbanki alkalmazásunk használata egyszerű és intuitív, így
+                bárki könnyedén használhatja.
+              </p>
+            </div>
+            <div className="advantage-card">
+              <img src="/path/to/advantage-icon-2.png" alt="Advantage Icon" />
+              <h6>Átláthatóság</h6>
+              <p>
+                Mindig tudni fogod, hogy mire költöttél, és mennyi pénzed maradt
+                a hónap végére.
+              </p>
+            </div>
+            <div className="advantage-card">
+              <img src="/path/to/advantage-icon-3.png" alt="Advantage Icon" />
+              <h6>Automatizált folyamatok</h6>
+              <p>
+                Állítsd be az alkalmazást, hogy automatikusan számolja ki a
+                bevételeidet és kiadásaidat.
+              </p>
+            </div>
+            
+            {/* Advantages buttons */}
+            <div className="advantages-buttons">
+              <button className="main_button">Tudj meg többet</button>
+              <button className="secundary_button">Regisztráció</button>
+            </div>
+          </div>
+        </div>
         
-      </>
+        {/* Advantages image */}
+        <div className="advantages-image">
+        <img src="/path/to/advantages-image.png" alt="Advantages Illustration" />
+      </div>
+      </section>
     )
 }
 export function TryItOut() {  
