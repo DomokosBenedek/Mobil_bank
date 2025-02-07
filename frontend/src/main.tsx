219dcc4bc4bde136701203791330b45746bba35f
Domokos Benedek <benedekdomokos50@gmail.com>
2025-02-07 09:17:32 +0100

    maps debug

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,36 +1,28 @@
 import {StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import HomePage from './pages/home'
-import Profile_List from './pages/Profile_List'
-import Profile from './pages/Profile'
-import Regist from './pages/regist'
-import Test from './components/test'
+import HomePage from './pages/home';
+import Profile_List from './pages/Profile_List';
+import Regist from './pages/regist';
+import Profile from './pages/Profile';
+//import Test from './components/test'
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage/>,
-  },
-  {
-    path: "/profile",
-    element: <Profile_List/>,
-  },
-  {
-    path: "/profile/:username",
-    element: <Profile/>,
-  },
-  {
-    path: "/Regist",
-    element: <Regist/>,
-  },
-]);
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/profile" element={<Profile_List />} />
+      <Route path="/profile/:username" element={<Profile />} />
+      <Route path="/Regist" element={<Regist />} />
+    </Routes>
+  </Router>
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
-)
+);
